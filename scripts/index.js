f05269701e0546fdfb9a6427362b2d4a6c0d082f
Your Name <mash.pogonina2014@yandex.ru>
2022-11-27 13:42:10 +0300

    feat: добавить работу с карточками через js

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,149 @@
+// переменные для popup
 const editButton = document.querySelector('.profile__edit-button');
-const popUp = document.querySelector('.popup');
-const closeButton = document.querySelector('.popup__close-button');
-
-const saveInfoProfile = document.querySelector('.form');
-const nameInput = document.querySelector('.form__input-name');
-const descriptionInput = document.querySelector('.form__input-description');
-
+const addButton = document.querySelector('.profile__add-button');
+const popUpProfile = document.getElementById('profile');
+const popUpAdd = document.getElementById('card');
+const closeButtonProfile = document.querySelector('#profile-close');
+const closeButtonAdd = document.querySelector('#card-close');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 
-function openPopUp() {
-  popUp.classList.add('popup_opened');
+// переменные для form editProfile
+const saveInfoProfile = document.querySelector('[name="editProfile"]');
+const nameInput = saveInfoProfile.querySelector('[name="name"]');
+const descriptionInput = saveInfoProfile.querySelector('[name="description"]');
+
+// переменные для form addCard
+const addCard = document.querySelector('[name="addCard"]');
+const titleInput = addCard.querySelector('[name="title"]');
+const linkInput = addCard.querySelector('[name="link"]');
+
+// переменные для Template
+const cardsListElement = document.querySelector('.cards');
+const cardTemplate = document.querySelector('#сard-template').content.querySelector('.card');
+
+// массив для cards
+const initialCards = [
+  {
+    name: 'Бразилия',
+    link: '../images/brazil.jpg'
+  },
+  {
+    name: 'Чехия',
+    link: '../images/czech.jpg'
+  },
+  {
+    name: 'Япония',
+    link: '../images/japan.jpg'
+  },
+  {
+    name: 'Финляндия',
+    link: '../images/finland.jpg'
+  },
+  {
+    name: 'Швейцария',
+    link: '../images/Switzerland.jpg'
+  },
+  {
+    name: 'Тайланд',
+    link: '../images/thailland.jpg'
+  }
+];
+
+
+// открытие PopUpProfile
+function openPopUpProfile() {
+  popUpProfile.classList.add('popup_opened');
 
   nameInput.value = `${profileName.textContent}`;
   descriptionInput.value = `${profileDescription.textContent}`;
 };
 
-function closePopUp() {
-  popUp.classList.remove('popup_opened');
+// открытие PopUpAdd
+function openPopUpAdd() {
+  popUpAdd.classList.add('popup_opened');
+
+  titleInput.value = "";
+  linkInput.value = "";
 };
 
-function formSubmitHandler (evt) {
+// закрытие PopUp
+function closePopUpProfile() {
+  popUpProfile.classList.remove('popup_opened');
+};
+
+function closePopUpAdd() {
+  popUpAdd.classList.remove('popup_opened');
+};
+
+// сохранение новый данных профиля
+function submitProfileInfo(evt) {
   evt.preventDefault();
 
   profileName.textContent = `${nameInput.value}`;
   profileDescription.textContent = `${descriptionInput.value}`;
 
 
-  closePopUp();
+  closePopUpProfile();
 }
 
-editButton.addEventListener('click', openPopUp);
-closeButton.addEventListener('click', closePopUp);
 
-saveInfoProfile.addEventListener('submit', formSubmitHandler);
+// создание cards из массива
+function createCards(item) {
+  const card = cardTemplate.cloneNode(true);
+  const likeButton = card.querySelector('.card__like');
+  const DelButton = card.querySelector('.card__trash');
+  const cardName = card.querySelector('.card__name');
+  const cardImg = card.querySelector('.card__img');
+
+  cardImg.src = item.link;
+  cardName.textContent = item.name;
+  cardImg.alt = item.name;
+
+  likeButton.addEventListener('click', clickCardLike);
+  DelButton.addEventListener('click', clickCardDel);
+
+  return card;
+}
+
+// лайк карточки
+const clickCardLike = (evt) => {
+  evt.target.classList.toggle('card__like_active');
+}
+// удаление карточки
+const clickCardDel = (evt) => {
+  evt.target.closest('.card').remove();
+}
+
+// прорисовка card
+const renderCard = (item, wrapElement) => {
+  const element = createCards(item);
+  wrapElement.prepend(element);
+}
+
+// добавление card на страницу
+initialCards.forEach(function (item) {
+  renderCard(item, cardsListElement);
+})
+
+// добавление карточки пользователем
+const submitAddCard = (evt) => {
+  evt.preventDefault();
+
+  const card = {
+    name: titleInput.value,
+    link: linkInput.value
+  };
+
+  renderCard(card, cardsListElement);
+  closePopUpAdd();
+}
+
+editButton.addEventListener('click', openPopUpProfile);
+addButton.addEventListener('click', openPopUpAdd);
+closeButtonProfile.addEventListener('click', closePopUpProfile);
+closeButtonAdd.addEventListener('click', closePopUpAdd);
+
+saveInfoProfile.addEventListener('submit', submitProfileInfo);
+addCard.addEventListener('submit', submitAddCard);
+
